b2a4eb2f8bb831debb128378b3d64eff5864d0c6
gxzpython <gxzpython@gmail.com>
2021-03-16 16:25:32 -0500

    this is the calculator project

---
+++ script.js
@@ -0,0 +1,23 @@
+// function insert(num) {
+//     document.form.textview.value = document.form.textview.value + num
+// }
+
+// function equal() {
+//     var exp = document.form.textview.value;
+//     if (exp) {
+//         document.form.textview.value = eval(exp)
+//         alert("The result is "+eval(exp))
+//     }
+
+// }
+
+// function back() {
+//     var exp = document.form.textview.value;
+//     document.form.textview.value = exp.substring(0, exp.length - 1);
+// }
+
+// function empty(){
+
+//     document.form.textview.value ="";
+
+// }
